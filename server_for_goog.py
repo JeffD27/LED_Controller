c9a0e233a3c89ef44505735e92263026262b3f7c
JeffD27 <jeffreydangelo1@gmail.com>
2023-11-26 15:26:48 -0500

    server

--- server_for_goog.py
+++ server_for_goog.py
@@ -1,13 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
 import os
 
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/prime', methods=['POST'])
-def switch():
-	query = request.json.get('obj',"NA")
-	os.system('sudo reboot')	
-	print(query, 'query')
-
+class Action(Resource):
+	def post(self, name):
+		print('working')
+		if(name == "backgroundlightsorange"):
+			print('rebooting!')
+			os.system("reboot")    
+        
+	def get(self, name):
+		print('here')
+		return  name
+api.add_resource(Action, "/action/<name>")
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0',debug=True,port=80)
+	
+	app.run(host= '0.0.0.0',debug=True)
